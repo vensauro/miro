879c2487032682872c3817d9249d64c26800dd17
Ivens <ivensjorisnbacon@gmail.com>
2020-09-30 23:05:04 -0300

    exemplo todo errado

--- lambda/index.js
+++ lambda/index.js
@@ -12,44 +12,44 @@ exports.handler = async (event, context) => {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
 
-  const params = querystring.parse(event.body);
-  //   console.log(params);
+  const params = JSON.parse(event.body);
+  console.log(params);
 
-  //   const mg = mailgun({
-  //     apiKey: MAILGUN_API_KEY,
-  //     domain: MAILGUN_DOMAIN,
-  //   });
-  //   try {
-  //     await mg.messages().send({
-  //       from: MAILGUN_FROM,
-  //       to: EMAIL_MIRO,
-  //       subject: 'Email do site matricula2021.colegiomiro.com.br',
-  //       html: `
-  //   <h1> Mensagem de ${params.nome} </h1>
-  //   <p> email: ${params.email} </p>
-  //   <p> telefone: ${params.telefone} </p>
-  //   <p> série pretendida: ${params.serie} </p>
-  //   <p> motivo: ${params.motivo} </p>
-  //   <p> mensagem: ${params.mensagem} </p>
-  //       `,
-  //     });
+  const mg = mailgun({
+    apiKey: MAILGUN_API_KEY,
+    domain: MAILGUN_DOMAIN,
+  });
+  try {
+    await mg.messages().send({
+      from: MAILGUN_FROM,
+      to: EMAIL_MIRO,
+      subject: 'Email do site matricula2021.colegiomiro.com.br',
+      html: `
+    <h1> Mensagem de ${params.nome} </h1>
+    <p> email: ${params.email} </p>
+    <p> telefone: ${params.telefone} </p>
+    <p> série pretendida: ${params.serie} </p>
+    <p> motivo: ${params.motivo} </p>
+    <p> mensagem: ${params.mensagem} </p>
+        `,
+    });
 
-  //     await mg.messages().send({
-  //       from: MAILGUN_FROM,
-  //       to: params.email,
-  //       subject: 'Confirmação de mensagem ao Colégio Miró',
-  //       html: `
-  // Olá ${params.nome}, confirmamos que sua mensagem foi enviada para o Colégio Miró. <br/>
-  // Agradecemos a paciência.
-  //     `,
-  //     });
-  //   } catch (error) {
-  //     console.error(error);
-  //     return {
-  //       statusCode: 500,
-  //       body: 'Error',
-  //     };
-  //   }
+    await mg.messages().send({
+      from: MAILGUN_FROM,
+      to: params.email,
+      subject: 'Confirmação de mensagem ao Colégio Miró',
+      html: `
+  Olá ${params.nome}, confirmamos que sua mensagem foi enviada para o Colégio Miró. <br/>
+  Agradecemos a paciência.
+      `,
+    });
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: 'Error',
+    };
+  }
 
   return {
     statusCode: 200,
